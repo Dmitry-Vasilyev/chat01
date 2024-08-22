ff1f4e82b1af88129fea6594f4ce7cb4bdfd04a2
DiVa <dimav.programming@gmail.com>
2024-08-22 18:26:24 +0300

    Init ws

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const http = require('http');
+const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const httpServer = http.createServer(app);
+const wss = new WebSocket.Server({ server: httpServer });
 
 httpServer.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
-
