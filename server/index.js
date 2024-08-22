b99d49cb2ccc4ad85073eb589d840d52db92cf4d
DiVa <dimav.programming@gmail.com>
2024-08-22 18:19:36 +0300

    Init Express

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
 require('dotenv').config();
+const express = require('express');
+const http = require('http');
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+const httpServer = http.createServer(app);
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
