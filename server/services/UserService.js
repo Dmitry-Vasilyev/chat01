887da8143ffc07ab23888bc4851349ea4368854b
DiVa <dimav.programming@gmail.com>
2024-09-03 20:52:55 +0300

    UserService add user to DB

--- server/services/UserService.js
+++ server/services/UserService.js
@@ -1,7 +1,25 @@
+const UserModel = require("../models/UserModel");
+const UserDto = require("../dtos/UserDto");
+
 class UserService {
-    constructor(hashUtil) {
+    constructor(hashUtil, tokenService) {
         this.hashUtil = hashUtil;
+        this.tokenService = tokenService;
     }
+    async registration (email, password, nickname) {
+        const candidate = await UserModel.findOne({email});
+        if (candidate) {
+            throw new Error(`User with email ${email} already exists`);
+        }
+
+        const hashedPassword = await this.hashUtil.hashPassword(password);
+
+        const user = await UserModel.create({email, password: hashedPassword, nickname});
+
+        const userDto = new UserDto(user);
+        console.log(this.tokenService.generateToken({userDto}));
+    }
+
 }
 
 module.exports = UserService;
