ab2ec9666df355e25a4cc47f7eac129ccd294e45
DiVa <dimav.programming@gmail.com>
2024-09-03 20:47:43 +0300

    TokenService method generateToken

--- server/services/TokenService.js
+++ server/services/TokenService.js
@@ -2,5 +2,12 @@ const TokenModel = require("../models/TokenModel");
 const jwt = require("jsonwebtoken");
 
 class TokenService {
+    generateToken(payload) {
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET);
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET);
 
+        return {accessToken, refreshToken};
+    }
 }
+
+module.exports = TokenService;
