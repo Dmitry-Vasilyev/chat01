a39b812854a8d294a4c6766a681b179a9f74e42a
DiVa <dimav.programming@gmail.com>
2024-09-09 20:44:34 +0300

    authRoutes add validation by using class AuthValidator for all endpoints

--- server/routers/authRoutes.js
+++ server/routers/authRoutes.js
@@ -1,19 +1,28 @@
 const Router = require('express').Router;
 
 class AuthRoutes {
-    constructor(userController) {
+    constructor(userController, authValidator) {
         this.authRouter = new Router();
         this.userController = userController;
+        this.authValidator = authValidator;
 
         this.setupRoutes();
     }
 
     setupRoutes() {
-        this.authRouter.post('/registration', this.userController.registration);
-        this.authRouter.post('/login', this.userController.login);
-        this.authRouter.post('/logout', this.userController.logout);
+        this.authRouter.post('/registration',
+            this.authValidator.registrationValRules(),
+            this.userController.registration);
+        this.authRouter.post('/login',
+            this.authValidator.loginValRules(),
+            this.userController.login);
+        this.authRouter.post('/logout',
+            this.authValidator.tokenValRules(),
+            this.userController.logout);
         this.authRouter.get('/activate/:link', this.userController.activate);
-        this.authRouter.get('/refresh', this.userController.refresh);
+        this.authRouter.get('/refresh',
+            this.authValidator.tokenValRules(),
+            this.userController.refresh);
         this.authRouter.get('/users', this.userController.getAllUsers);
     }
 
